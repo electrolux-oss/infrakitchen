25172d154b576a440804d41dfc625aeaf0a4e74d
Vladimir Shcherbinin <dester.irk@gmail.com>
2025-11-13 11:26:21 +0100

    check column type on sorting

--- server/src/core/utils/fastapi_tools.py
+++ server/src/core/utils/fastapi_tools.py
@@ -13,8 +13,11 @@ def parse_query_params(
     sort: str = Query(None),
     fields: str = Query(None),
 ) -> QueryParamsType:
-    parsed_filter = json.loads(filter) if filter else None
-    parsed_range = tuple(json.loads(range)) if range else None
-    parsed_sort = tuple(json.loads(sort)) if sort else None
-    parsed_fields = json.loads(fields) if fields else None
-    return parsed_filter, parsed_range, parsed_sort, parsed_fields
+    try:
+        parsed_filter = json.loads(filter) if filter else None
+        parsed_range = tuple(json.loads(range)) if range else None
+        parsed_sort = tuple(json.loads(sort)) if sort else None
+        parsed_fields = json.loads(fields) if fields else None
+        return parsed_filter, parsed_range, parsed_sort, parsed_fields
+    except json.JSONDecodeError as e:
+        raise ValueError("Invalid query parameter format") from e
