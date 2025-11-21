8f60424f9a1ba2fff9f3563f5e76cd8d6af9dd84
Ramil Galimov <dev.rgalim@gmail.com>
2025-11-21 11:35:54 +0100

    add create role page

--- ui/frontend-lib/src/roles/routes.ts
+++ ui/frontend-lib/src/roles/routes.ts
@@ -1,13 +1,18 @@
 import { RolePage } from "./pages/Role";
 import { RolesPage } from "./pages/Roles";
+import { RoleCreatePage } from "./pages/RoleCreate";
 
 export const roleRoutes = [
   {
-    path: RolePage.path,
-    Component: RolePage,
+    path: RoleCreatePage.path,
+    Component: RoleCreatePage,
   },
   {
     path: RolesPage.path,
     Component: RolesPage,
+  },
+  {
+    path: RolePage.path,
+    Component: RolePage,
   },
 ];
