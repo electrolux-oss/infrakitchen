0faea52d4c807fe73d9eadc63a5ae95a0ed961f4
Török István-Dániel <63093503+torokistvan06@users.noreply.github.com>
2026-02-24 19:08:47 +0200

    Disable new tag for versions (#103)

--- ui/frontend-lib/src/source_codes/components/SourceCodeRefRow.tsx
+++ ui/frontend-lib/src/source_codes/components/SourceCodeRefRow.tsx
@@ -447,10 +447,11 @@ export const SourceCodeRefRow = ({
     setActiveTab(newValue);
   };
 
-  const isRecentlyCreated =
-    hasVersion &&
-    entity?.created_at &&
-    isAfter(new Date(entity.created_at), subDays(new Date(), 3));
+  // TODO
+  // const isRecentlyCreated =
+  //   hasVersion &&
+  //   entity?.created_at &&
+  //   isAfter(new Date(entity.created_at), subDays(new Date(), 3));
 
   return (
     <Paper
@@ -487,6 +488,7 @@ export const SourceCodeRefRow = ({
           <Typography variant="body2" sx={{ fontSize: "0.82rem" }}>
             {entry}
           </Typography>
+          {/* TO DO
           {(!hasVersion || isRecentlyCreated) && (
             <Chip
               label="New"
@@ -498,7 +500,7 @@ export const SourceCodeRefRow = ({
                 "& .MuiChip-label": { px: 0.75 },
               }}
             />
-          )}
+          )} */}
         </Box>
 
         <Box sx={{ display: "flex", alignItems: "center" }}>
