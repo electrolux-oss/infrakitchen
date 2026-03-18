32477b712b86334d4200e7372ce0d0e84787fc15
Szabó Ágnes-Kriszta <agnes_kriszta@yahoo.com>
2026-03-18 14:18:09 +0200

    Switch to tabbed content on Template page

--- ui/frontend-lib/src/templates/components/TemplateContent.tsx
+++ ui/frontend-lib/src/templates/components/TemplateContent.tsx
@@ -1,6 +1,10 @@
 import { Box } from "@mui/material";
 
 import { DangerZoneCard } from "../../common/components/DangerZoneCard";
+import {
+  TabbedContent,
+  TabDefinition,
+} from "../../common/components/TabbedContent";
 import { EntityTreeView } from "../../common/components/tree/TreeView";
 import { useEntityProvider } from "../../common/context/EntityContext";
 
@@ -11,20 +15,37 @@ export const TemplateContent = () => {
   const { entity } = useEntityProvider();
   if (!entity) return null;
 
+  const tabs: TabDefinition[] = [
+    {
+      label: "Resources",
+      content: <TemplateResources template_id={entity.id} />,
+    },
+    {
+      label: "Tree View",
+      content: (
+        <EntityTreeView
+          entity_id={entity.id}
+          entity_name={entity._entity_name}
+        />
+      ),
+    },
+    {
+      label: "Settings",
+      content: <DangerZoneCard />,
+      requiredPermission: `template:${entity.id}`,
+      permissionAction: "write",
+    },
+  ];
+
   return (
     <Box
       sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
         gap: 2,
         minWidth: 1000,
       }}
     >
       <TemplateOverview template={entity} />
-      <TemplateResources template_id={entity.id} />
-      <EntityTreeView entity_id={entity.id} entity_name={entity._entity_name} />
-      <DangerZoneCard />
+      <TabbedContent tabs={tabs} />
     </Box>
   );
 };
